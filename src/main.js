521bdb46e3cc863e0ffc45f039c621cee2cb9e13
jf7689 <jf7689@rit.edu>
2020-10-22 19:58:59 -0400

    Removed some useless comments and code

--- src/main.js
+++ src/main.js
@@ -134,39 +134,9 @@ function setupUI(canvasElement) {
 } // end setupUI
 
 function loop() {
-    /* NOTE: This is temporary testing code that we will delete in Part II */
     requestAnimationFrame(loop);
     canvas.draw(drawParams);
 
-    /* 
-
-    // 1) create a byte array (values of 0-255) to hold the audio data
-    // normally, we do this once when the program starts up, NOT every frame
-    let audioData = new Uint8Array(audio.analyserNode.fftSize/2);
-    
-    // 2) populate the array of audio data *by reference* (i.e. by its address)
-    audio.analyserNode.getByteFrequencyData(audioData);
-    //audio.analyserNode.getByteTimeDomainData(audioData); // waveform data
-    
-    // 3) log out the array and the average loudness (amplitude) of all of the frequency bins
-        console.log(audioData);
-        
-        console.log("-----Audio Stats-----");
-        let totalLoudness =  audioData.reduce((total,num) => total + num);
-        let averageLoudness =  totalLoudness/(audio.analyserNode.fftSize/2);
-        let minLoudness =  Math.min(...audioData); // ooh - the ES6 spread operator is handy!
-        let maxLoudness =  Math.max(...audioData); // ditto!
-        // Now look at loudness in a specific bin
-        // 22050 kHz divided by 128 bins = 172.23 kHz per bin
-        // the 12th element in array represents loudness at 2.067 kHz
-        let loudnessAt2K = audioData[11]; 
-        console.log(`averageLoudness = ${averageLoudness}`);
-        console.log(`minLoudness = ${minLoudness}`);
-        console.log(`maxLoudness = ${maxLoudness}`);
-        console.log(`loudnessAt2K = ${loudnessAt2K}`);
-        console.log("---------------------"); 
-
-        */
 }
 
 export { init };
